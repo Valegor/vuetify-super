f458df51b9ad5b4dace3e6e8a6cfe4094b2e430d
Valegor <medifit2000@yandex.ru>
2022-01-19 21:40:10 +0300

    game categoryes page

--- src/components/Game/GameCategories.vue
+++ src/components/Game/GameCategories.vue
@@ -1,28 +1,74 @@
 <template>
-    <v-container>
-        <v-row justify="center">
-            Games Categories
-        </v-row>
-    </v-container>
+
+<div>
+
+<v-card
+    class="mx-auto mb-4 indigo lighten-4"
+    max-width="400"
+    tile
+>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title
+        class="h5"
+        >
+        ДОСТУПНЫЕ КАТЕГОРИИ ИГР
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+
+  <v-card
+    v-for="category in categories" :key="category.id"
+    class="mx-auto mb-4"
+    max-width="400"
+    tile
+    :to="'/games-category/' + category.id"
+  >
+    <v-list-item>
+        <v-list-item-content>
+            <v-list-item-title>{{ category.category }}</v-list-item-title>
+        </v-list-item-content>
+    </v-list-item> 
+  </v-card>
+
+ </div> 
+
 </template>
 
 <script>
+    import axios from '@/axios/axios'
 
     export default {
     data: () => ({
-
+        categories: ''
     }),
     methods: {
+        async getGameCategory(){
 
+            console.log('start')
+
+            await axios
+                .get("/api/game-categories/")
+                .then(response => {
+                    this.categories = response.data.data;
+                })
+                .catch(function(e){
+                    console.log('error')
+                    this.error = e;
+                });
+
+            // this.cat = this.category[0];
+
+
+
+        }
     },
-    created () {
-
-    }, 
     updated (){
-
+        // this.getGameCategory()
     },
     mounted() {
-
+        this.getGameCategory()
     }
 
     }
