c3e95a0da7d55ae99bf7214eddcb978299daea81
Valegor <medifit2000@yandex.ru>
2022-01-21 22:24:11 +0300

    add card detail

--- src/components/Game/Card.vue
+++ src/components/Game/Card.vue
@@ -1,28 +1,133 @@
-<template>
-    <v-container>
-    <v-row justify="center">
-        Card
-    </v-row>
-    </v-container>
+
+<template v-slot:card.notes="{{card.notes}}">
+
+<div>
+
+  <v-card
+    class="mx-auto"
+    max-width="455"
+  >
+    <v-img
+      :src="this.imageUrl"
+      max-height="450"
+      contain
+      @click="this.changeImg"
+    ></v-img>
+
+    <v-card-title>
+      {{ card.name }}
+    </v-card-title>
+
+    <hr>
+
+    <v-card-subtitle>
+      <b>Category:</b> {{ card.category }} <br> 
+      <hr>
+          <v-img
+                :src="this.image3Url"
+                max-height="225"
+                contain
+            ></v-img>     
+    </v-card-subtitle>
+
+
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        Read More
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+
+   <div v-html="card.notes"></div>
+
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+
+
+ </div> 
+
 </template>
 
 <script>
 
+    import axios from '@/axios/axios'
+
     export default {
     data: () => ({
-
+        card: '',
+        show: false,
+        image1Url: '',
+        image2Url: '',
+        image3Url: '',
+        imageUrl: ''
     }),
     methods: {
+        async getPost(){
 
+
+            this.card_id = this.$route.params.id
+
+            console.log(this.card_id)
+
+            // console.log(this.$store.getters.serverUrl)
+
+            
+            await axios
+                .get('/api/card/' + this.card_id 
+                //{
+                // params: {
+                //         id: this.cat_id
+                //     }
+                // }
+                )
+                .then(response => {
+                    this.card = response.data[0];
+                })
+                .catch(function(e){
+                    // console.log('error')
+                    this.error = e;
+                });
+
+                this.image1Url = this.$store.getters.serverUrl + this.card.img1
+                this.image2Url = this.$store.getters.serverUrl + this.card.img2 
+                this.image3Url = this.$store.getters.serverUrl + this.card.backimg
+
+                this.imageUrl = this.image1Url
+
+        },
+        changeImg(){
+            this.imageUrl = this.image2Url
+            setTimeout(() => {  this.imageUrl = this.image1Url; }, 700);
+        }
     },
-    created () {
-
-    }, 
+    created() {
+        this.getPost()
+    },
     updated (){
-
+        // this.getGameCategory()
     },
     mounted() {
-
+        // this.getGameCategory()
     }
 
     }
--- src/components/Game/CardsCategory.vue
+++ src/components/Game/CardsCategory.vue
@@ -1,28 +1,85 @@
+
 <template>
-    <v-container>
-    <v-row justify="center">
-        Cards Category
-    </v-row>
-    </v-container>
+
+<div>
+
+<v-card
+    :to="'/card/' + card.id"
+    v-for="card in cards.data" :key="card.id"
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      :src="serverUrl + card.img1"
+      max-height="450"
+      contain
+    ></v-img>
+
+    <v-card-title>
+      Подробнее о карте>>
+    </v-card-title>
+
+      <v-spacer></v-spacer>
+
+
+    <br>
+
+  </v-card>
+  
+  
+
+  <br>
+
+<pagination 
+    :data="cards" 
+    @pagination-change-page="getResults"
+    :limit=2
+    size="small"
+    align="center"
+></pagination>
+
+</div> 
+
 </template>
 
 <script>
 
-    export default {
-    data: () => ({
+    import axios from '@/axios/axios'
 
-    }),
+    export default {
+    data () {
+        return {
+            cards: {},
+            page: 1,
+            cat_id: '',
+            show: false,
+            serverUrl: ''
+
+        }
+    },
     methods: {
+        getResults(page = this.page) {
+
+            this.cat_id = this.$route.params.id
+
+            // console.log(this.cat_id)
+
+			axios.get('/api/card-category/' + + this.cat_id + '?page=' + page)
+				.then(response => {
+					this.cards = response.data;
+				});
+            
+            this.serverUrl = this.$store.getters.serverUrl;
+		}
+    },
+    created() {
 
     },
-    created () {
-
-    }, 
     updated (){
 
     },
     mounted() {
-
+        this.getResults()
     }
 
     }
