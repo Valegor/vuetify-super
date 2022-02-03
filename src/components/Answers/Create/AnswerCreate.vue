91ced308af01835d30c4d2ba7da33ebe64edca86
Valegor <medifit2000@yandex.ru>
2022-02-03 14:33:01 +0300

    add check answer

--- src/components/Answers/Create/AnswerCreate.vue
+++ src/components/Answers/Create/AnswerCreate.vue
@@ -8,20 +8,65 @@
 
 <script>
 
+    import axios from '@/axios/axios'
+
     export default {
     data: () => ({
-
+        exist_answer: '',
+        author_email: '',
+        template_id: '',
     }),
     methods: {
+        start(){
+            console.log('start create template')
+            this.exist()
+        },
+        create(){
+
+            console.log('create')
+
+
+        },
+        edit(){
+
+            console.log('edit')
+
+
+        },
+        exist(){
+            
+            this.template_id = this.$route.params.id
+            this.author_email = this.$store.getters.USER_EMAIL
+
+            //проверить есть ли у этого автора незавершенный ответ на дынный шаблон
+
+            axios.post('/api/answer-check', { author_email: this.author_email, template_id: this.template_id })
+                    .then( response => {
+                        this.exist_answer = response.data
+
+                        console.log(this.exist_answer)
+
+                        if(this.exist_answer == 1){
+                            this.edit()
+                        } else { this.create() }
+
+                        return response
+                    })
+                    .catch( err => {
+                        // console.log('error')
+                        console.log(err.response);
+                    })  
+        }
+
     },
     created () {
-
+        
     }, 
     updated (){
-
+    
     },
     mounted() {
-
+        this.start()
     }
 
     }
