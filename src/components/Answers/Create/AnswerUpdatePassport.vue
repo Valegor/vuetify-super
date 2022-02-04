366e77bfdbc5137dab8975cdddebf896db46a1ed
Valegor <medifit2000@yandex.ru>
2022-02-04 09:36:36 +0300

    tuning form edit passport

--- src/components/Answers/Create/AnswerUpdatePassport.vue
+++ src/components/Answers/Create/AnswerUpdatePassport.vue
@@ -8,6 +8,7 @@
         <v-textarea
           filled
           auto-grow
+          v-model="title"
           label="Title"
           rows="1"
           row-height="20"
@@ -20,8 +21,8 @@
       filled
       rows="3"
       label="Subtitle"
+      v-model="subtitle"
       auto-grow
-      value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
     ></v-textarea>
 </v-container>
 
@@ -31,8 +32,9 @@
       name="input-7-1"
       filled
       label="Notes"
+      v-model="notes"
       auto-grow
-      value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+      value=""
     ></v-textarea>
 </v-container>
 
@@ -59,16 +61,42 @@
 
 <script>
 
+    import axios from '@/axios/axios'
+
     export default {
     data: () => ({
-
+        id: '',
+        answer: '',
+        title: '',
+        subtitle: '',
+        notes: '',
     }),
     methods: {
+        start(){
+            console.log('start')
+            this.getAnswer()
+        },
         onSubmit(){
 
         },
         editObject(){
 
+        },
+        getAnswer(){
+            
+            this.answer_id = this.$route.params.id
+            
+            axios.get('/api/answer/' + this.answer_id)
+                .then(response => {
+                    this.answer = response.data[0];
+                    this.id = this.answer.id
+                    this.title = this.answer.title
+                    this.subtitle = this.answer.subtitle
+                    this.notes = this.answer.notes
+                })
+                .catch(function(e){
+                    this.error = e;
+                });              
         },
     },    
     created () {
@@ -78,7 +106,7 @@
 
     },
     mounted() {
-
+        this.start()
     }
     }
 
