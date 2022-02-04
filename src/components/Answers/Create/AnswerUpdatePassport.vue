79d2849c7679085254544a6ec819c7980cf039e4
Valegor <medifit2000@yandex.ru>
2022-02-04 08:48:49 +0300

    add form edit passport

--- src/components/Answers/Create/AnswerUpdatePassport.vue
+++ src/components/Answers/Create/AnswerUpdatePassport.vue
@@ -1,9 +1,60 @@
 <template>
-    <v-container>
-        <v-row justify="center">
-            ANSWER-UPDATE-PASSPORT
-        </v-row>
-    </v-container>
+<div>
+    <v-form
+    ref="form"
+  >
+
+<v-container fluid>
+        <v-textarea
+          filled
+          auto-grow
+          label="Title"
+          rows="1"
+          row-height="20"
+        ></v-textarea>
+  </v-container>
+
+  <v-container fluid>
+    <v-textarea
+      name="input-7-1"
+      filled
+      rows="3"
+      label="Subtitle"
+      auto-grow
+      value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+    ></v-textarea>
+</v-container>
+
+
+<v-container fluid>
+    <v-textarea
+      name="input-7-1"
+      filled
+      label="Notes"
+      auto-grow
+      value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+    ></v-textarea>
+</v-container>
+
+
+
+<v-container fluid>
+    <v-btn
+      color="success"
+      class="mr-4"
+      @click="onSubmit"
+    >
+      Save
+    </v-btn>
+
+      <v-btn @click="editObject">
+        Edit Object
+      </v-btn>
+
+</v-container>
+
+  </v-form>
+</div>
 </template>
 
 <script>
@@ -13,8 +64,13 @@
 
     }),
     methods: {
+        onSubmit(){
 
-    },
+        },
+        editObject(){
+
+        },
+    },    
     created () {
 
     }, 
